cdf97ac0ab21888e98ce6de6f320d450b8845bed
Aram Khachatryan <dyooreen@icloud.com>
2024-05-20 16:46:07 +0400

    render children categories

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+function renderChildCategories(children) {
+  children.each(function () {
+    const subChildren = JSON.parse($(this).data("sub").split("'").join('"'));
+    x(subChildren, this);
+  });
+}
+
+function isMultiLevel(item) {
+  return item.name ? false : true;
+}
+function x(children, self) {
+  const ul = $("<ul>");
+  children.map((child) => {
+    let text = isMultiLevel(child) ? Object.keys(child)[0] : child.name;
+    let li = $(`<li>${text}</li>`);
+    $(ul).append(li).appendTo(self);
+
+    if (isMultiLevel(child)) {
+      x(child[text], li);
+    }
+  });
+}
+renderChildCategories($(`.category-btn`));
